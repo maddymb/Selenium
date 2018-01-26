2ec7ac6ee94a9c836e57e1c83c0f57c23dfc2418
Madhur Bharadwaj <maddy@Maddy.local>
2018-01-26 12:50:32 -0600

    first git commit

---
+++ src/com/selenium/Sample.java
@@ -0,0 +1,22 @@
+package com.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.setProperty("webdriver.chrome.driver", "/Users/maddy/Downloads/chromedriver");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://google.com");
+		//driver.quit();
+		
+		
+	}
+
+}
